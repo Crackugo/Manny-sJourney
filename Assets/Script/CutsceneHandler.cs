3558385023b8135f1ec0080e400c238f20cee125
Hugo <hfralmeida@gmail.com>
2024-06-06 19:49:04 +0100

    Tubo

--- Assets/Script/CutsceneHandler.cs
+++ Assets/Script/CutsceneHandler.cs
@@ -8,7 +8,14 @@ public class CutsceneHandler : MonoBehaviour
     public MonsterScript key;
     public GameObject parede;
     public StackChildren stack;
+    public GameObject door;
 
+    private Vector3 originalPosition;
+
+    public void Start(){
+        originalPosition = door.transform.position;
+    }
+ 
     public void StartKilling(){
             monster.ElevateBy(5);
             monster.startKilling(0.1f,1.0f);
@@ -19,12 +26,18 @@ public class CutsceneHandler : MonoBehaviour
     public void ResetEverything(){
         monster.ResetPosition();
         key.ResetPosition();
+        stack.Reset();
+        door.transform.position=   originalPosition;
         parede.transform.position= new Vector3(-9,167,69);
+        monster.followSpeed=9;
     }
 
     public void ChangeStairs(){
         stack.ChangeStairs();
+        door.transform.position=  originalPosition + new Vector3(0,-5,0);
         monster.transform.position=new Vector3(167,210,-27.25f);
+        monster.followSpeed/=2;
+
     }
 
 
